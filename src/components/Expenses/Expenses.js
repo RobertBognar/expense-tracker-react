1f61ed0b24717c389575c6057d1c5d9bdbfc4b49
RobertBognar <robertbognar2@gmail.com>
2021-11-30 10:08:44 +0100

    enabled filter by year button and refactoring jsx snippet code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,6 +11,25 @@ function Expenses(props) {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
+
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    //Message & Filter Method
+    let expensesContent = <p>No Expenses Found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ));
+    }
+
     return (
         <div>
             <Card className="expenses">
@@ -18,14 +37,7 @@ function Expenses(props) {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {props.items.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
+                {expensesContent}
             </Card>
         </div>
     );
